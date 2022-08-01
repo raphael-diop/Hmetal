3c17d4be28e33bd8bda131ea4e478b45aa3ffe57
Raphaël Diop <raphael.diop@laplateforme.io>
2022-08-01 21:58:12 +0200

    Query sécurisées

--- back/backRdv.php
+++ back/backRdv.php
@@ -17,17 +17,21 @@ require "Bdd.php";
         function verifValidDate($descriptif, $dateselect){
             $id_utilisateurs = $_SESSION['user']['id'];
 
-            $request = "SELECT `date` FROM `dates` WHERE `date` = '$dateselect'";
+            $request = "SELECT `date` FROM `dates` WHERE `date` = :dateselect";
             $calcul = $this->Bdd->prepare($request);
-            $calcul -> execute();
+            $calcul -> execute([':dateselect' => $dateselect]);
             $result = $calcul->rowcount();
             
  
 
             if($result == 1){
-                $request = "INSERT INTO `rdv`(`date`, `descriptif`, `id_utilisateurs`) VALUES ('$dateselect','$descriptif','$id_utilisateurs')";
+                $request = "INSERT INTO `rdv`(`date`, `descriptif`, `id_utilisateurs`) VALUES (:dateselect,:descriptif,:id_utilisateurs)";
                 $calcul = $this->Bdd->prepare($request);
-                $calcul -> execute();
+                $calcul -> execute([
+                    ':dateselect' => $dateselect,
+                    ':descriptif' => $descriptif,
+                    ':id_utilisateurs' => $id_utilisateurs,
+                ]);
 
                 $request2 = "SELECT*FROM `rdv`";
                 $calcul2 = $this->Bdd->prepare($request2);
@@ -44,9 +48,9 @@ require "Bdd.php";
 
         function recupRdv(){
             $id_utilisateurs = $_SESSION['user']['id'];
-            $request = "SELECT*FROM `rdv` WHERE `id_utilisateurs` = '$id_utilisateurs'";
+            $request = "SELECT*FROM `rdv` WHERE `id_utilisateurs` = :id_utilisateurs";
             $calcul = $this->Bdd->prepare($request);
-            $calcul -> execute();
+            $calcul -> execute([':id_utilisateurs' => $id_utilisateurs]);
             $result = $calcul->fetch(PDO::FETCH_ASSOC);
 
             return $result;
@@ -70,8 +74,11 @@ require "Bdd.php";
                     if($result2 = $date){
                         $date = implode($date);
                         $dispo = 2;
-                        $blockdate = $this->Bdd->prepare("UPDATE `dates` SET `date`='$date',`dispo`= '$dispo' WHERE date = '$date'");
-                        $blockdate->execute();
+                        $blockdate = $this->Bdd->prepare("UPDATE `dates` SET `date`= :date ,`dispo`= :dispo WHERE date = :date");
+                        $blockdate->execute([
+                            ':date' => $date,
+                            ':dispo' => $dispo,
+                        ]);
                     }
                 }
             }
